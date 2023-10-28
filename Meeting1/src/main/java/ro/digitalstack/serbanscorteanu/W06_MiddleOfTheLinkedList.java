94b3192d064277758684d846f9817e9b1d9e2785
Serban_Scorteanu <serban.scorteanu@dell.com>
2023-10-28 14:23:04 +0300

    middle of linkedlist

---
+++ Meeting1/src/main/java/ro/digitalstack/serbanscorteanu/W06_MiddleOfTheLinkedList.java
@@ -0,0 +1,39 @@
+package ro.digitalstack.serbanscorteanu;
+
+public class W06_MiddleOfTheLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
+    }
+}
